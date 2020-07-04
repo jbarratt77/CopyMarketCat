da56d2aa31708147f10cde669d526378d4cf3a6c
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2020-07-04 15:29:32 +0100

    :sparkles: add logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
+import logo from "./img/jaguar_logo.png"
 
 function App() {
-  return <Button color="primary">Hello World</Button>;
+  return (
+    <div>
+      <img src={logo} alt="jaguar logo" width="100"/>
+      <Button color="primary">Hello World</Button>
+    </div>
+  );
 }
 
 export default App;
