bfc15d299a0851a9790c806d33d39e3c3fe07347
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2020-07-05 10:28:29 +0100

    :sparkles: add wordpress plugin for wpcom

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -27,6 +27,18 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        // your WordPress source
+        baseUrl: `coindigger1.wordpress.com/`,
+        protocol: `https`,
+        // is it hosted on wordpress.com, or self-hosted?
+        hostingWPCOM: true,
+        // does your site use the Advanced Custom Fields Plugin?
+        useACF: false
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
