46a6222b9d4c3e4bd229bab66f34743664fb782d
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2020-07-05 10:00:48 +0100

    Update site title and description

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `CopyMarketCat`,
+    description: `A Coin Digger blog and copy cat site of CoinMarketCap for cryptocurrency news, exchange rates and opinions.`,
+    author: `@coin_digger`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
